ff4e5ee24eb8a2645f8b060e8a358ce2bd079c4f
GromkovAlexander <gromkovalex@mail.ru>
2019-10-24 19:59:55 +0300

    Scripted auto-commit on change (2019-10-24 19:59:55) by gitwatch.sh

--- src/main/java/bmstu/lab2/AirportJoinApp.java
+++ src/main/java/bmstu/lab2/AirportJoinApp.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AirportJoinApp {
@@ -20,6 +21,7 @@ public class AirportJoinApp {
 //        //Переделать
 //        FileInputFormat.addInputPath(job, new Path(args[0]));
 //        FileInputFormat.addInputPath(job, new Path(args[1]));
+        
 
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
