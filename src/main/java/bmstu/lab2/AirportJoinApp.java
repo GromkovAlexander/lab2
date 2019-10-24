e1ba8a5e4a5aec539fcd4616fb884aa15d138d82
GromkovAlexander <gromkovalex@mail.ru>
2019-10-24 19:43:00 +0300

    Scripted auto-commit on change (2019-10-24 19:43:00) by gitwatch.sh

--- src/main/java/bmstu/lab2/AirportJoinApp.java
+++ src/main/java/bmstu/lab2/AirportJoinApp.java
@@ -1,21 +1,36 @@
 package bmstu.lab2;
 
-public class App {
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class AirportJoinApp {
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: WordCountApp <input path> <output path>");
+        if (args.length != 3) {
+            System.err.println("Usage: AirportJoinApp <flights file path> <airports file path> <output path>");
             System.exit(-1);
         }
         Job job = Job.getInstance();
-        job.setJarByClass(WordCountApp.class);
-        job.setJobName("Word count");
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setMapperClass(WordMapper.class);
-        job.setReducerClass(WordReducer.class);
+        job.setJarByClass(AirportJoinApp.class);
+        job.setJobName("AirportJoinApp count");
+
+//        //Переделать
+//        FileInputFormat.addInputPath(job, new Path(args[0]));
+//        FileInputFormat.addInputPath(job, new Path(args[1]));
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+//        job.setMapperClass(WordMapper.class); //Mapper
+//        job.setReducerClass(WordReducer.class); //Reducer
+
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+        
         job.setNumReduceTasks(2);
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
