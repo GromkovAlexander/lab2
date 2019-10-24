2b4b2fe707ef2f5e272d210c23786b16361944d9
GromkovAlexander <gromkovalex@mail.ru>
2019-10-24 20:31:41 +0300

    Scripted auto-commit on change (2019-10-24 20:31:41) by gitwatch.sh

--- src/main/java/bmstu/lab2/testClass.java
+++ src/main/java/bmstu/lab2/testClass.java
@@ -5,14 +5,13 @@ import java.util.Arrays;
 import au.com.bytecode.opencsv.CSVReader;
 
 public class testClass {
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         //Build reader instance
         //Read data.csv
         //Default seperator is comma
         //Default quote character is double quote
         //Start reading from line number 2 (line numbers start from zero)
-        CSVReader reader = new CSVReader(new FileReader("data.csv"), ',' , '"' , 1);
+        CSVReader reader = new CSVReader(new FileReader("L_AIRPORT_ID.csv"), ',' , '"' , 1);
         //Read CSV line by line and use the string array as you want
         String[] nextLine;
         while ((nextLine = reader.readNext()) != null) {
