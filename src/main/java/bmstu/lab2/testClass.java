6b303998a6a13e89c00cf53d02a8c19168da8326
GromkovAlexander <gromkovalex@mail.ru>
2019-10-24 20:29:54 +0300

    Scripted auto-commit on change (2019-10-24 20:29:53) by gitwatch.sh

--- src/main/java/bmstu/lab2/testClass.java
+++ src/main/java/bmstu/lab2/testClass.java
@@ -1,7 +1,21 @@
 package bmstu.lab2;
 
 public class testClass {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws Exception
+    {
+        //Build reader instance
+        //Read data.csv
+        //Default seperator is comma
+        //Default quote character is double quote
+        //Start reading from line number 2 (line numbers start from zero)
+        CSVReader reader = new CSVReader(new FileReader("data.csv"), ',' , '"' , 1);
+        //Read CSV line by line and use the string array as you want
+        String[] nextLine;
+        while ((nextLine = reader.readNext()) != null) {
+            if (nextLine != null) {
+                //Verifying the read data here
+                System.out.println(Arrays.toString(nextLine));
+            }
+        }
     }
 }
