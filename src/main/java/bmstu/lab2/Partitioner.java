58aea3e91dc399ac86e3db05b807469501971181
GromkovAlexander <gromkovalex@mail.ru>
2019-10-26 01:20:33 +0300

    Scripted auto-commit on change (2019-10-26 01:20:33) by gitwatch.sh

--- src/main/java/bmstu/lab2/Partitioner.java
+++ src/main/java/bmstu/lab2/Partitioner.java
@@ -4,8 +4,8 @@ package bmstu.lab2;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class Partitioner<K, V> extends Partitioner<AirportIndicator, Text> {
-    public int getPartition(K key, V value, int numReduceTasks) {
+public class Partitioner<AirportIndicator, Text> extends Partitioner<AirportIndicator, Text> {
+    public int getPartition(AirportIndicator key, Text value, int numReduceTasks) {
         return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
     }
 }
