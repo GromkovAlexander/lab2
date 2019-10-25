58692e93fc356c9402987c25af1eec18ba8f7229
GromkovAlexander <gromkovalex@mail.ru>
2019-10-26 01:47:07 +0300

    Scripted auto-commit on change (2019-10-26 01:47:07) by gitwatch.sh

--- src/main/java/bmstu/lab2/FlightsReducer.java
+++ src/main/java/bmstu/lab2/FlightsReducer.java
@@ -1,16 +1,19 @@
 package bmstu.lab2;
 
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.MapOutputCollector;
+import org.apache.hadoop.mapreduce.Reducer;
+
 
 import java.io.IOException;
 import java.util.Iterator;
 
-public class FlightsReducer extends FlightsReducer<AirportIndicator, Text, Text, Text> {
+public class FlightsReducer extends Reducer<AirportIndicator, Text, IntWritable, Text> {
+
     @Override
-    protected void reduce(AirportIndicator key, Iterable<Text> values, MapOutputCollector.Context context) throws
-            IOException, InterruptedException {
+    protected void reduce(AirportIndicator key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
         Iterator<Text> iter = values.iterator();
         Text systemInfo = new Text(iter.next());
         while (iter.hasNext()) {
