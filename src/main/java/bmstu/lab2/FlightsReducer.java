ba26ff4d051ffb06ce7a04f7df38a49e4776a6fb
GromkovAlexander <gromkovalex@mail.ru>
2019-10-26 02:10:23 +0300

    Scripted auto-commit on change (2019-10-26 02:10:23) by gitwatch.sh

--- src/main/java/bmstu/lab2/FlightsReducer.java
+++ src/main/java/bmstu/lab2/FlightsReducer.java
@@ -13,7 +13,7 @@ public class FlightsReducer extends Reducer<AirportIndicator, Text, IntWritable,
 
     @Override
     protected void reduce(AirportIndicator key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        
+
         float min = Float.MAX_VALUE;
         float max = Float.MIN_VALUE;
         float sum = 0;
@@ -39,7 +39,7 @@ public class FlightsReducer extends Reducer<AirportIndicator, Text, IntWritable,
 
         if (count > 0) {
             float averageDelay = sum / count;
-            Text out = new Text(firstOutPart + " Minimum delay: " + min + " Maximum delay: " + max + " Average delay: " + averageDelay )
+            Text out = new Text(firstOutPart + " Minimum delay: " + min + " Maximum delay: " + max + " Average delay: " + averageDelay);
             context.write(new IntWritable(key.getKey()), out);
         }
 
