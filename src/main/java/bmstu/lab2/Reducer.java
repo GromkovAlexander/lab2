35338129600674b0b8456bb7f67f0445e96f99f5
GromkovAlexander <gromkovalex@mail.ru>
2019-10-26 01:42:54 +0300

    Scripted auto-commit on change (2019-10-26 01:42:54) by gitwatch.sh

--- src/main/java/bmstu/lab2/Reducer.java
+++ src/main/java/bmstu/lab2/Reducer.java
@@ -1,4 +1,18 @@
 package bmstu.lab2;
 
-public class Reducer {
+
+import org.apache.hadoop.io.Text;
+
+public class Reducer extends Reducer<AirportIndicator, Text, Text, Text> {
+    @Override
+    protected void reduce(AirportIndicator key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text systemInfo = new Text(iter.next());
+        while (iter.hasNext()) {
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
+            context.write(key.getFirst(), outValue);
+        }
+    }
 }
