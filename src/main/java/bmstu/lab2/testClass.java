c815ca0b68f3ccbffbbbae9abf6c59dfbc5ae1bc
GromkovAlexander <gromkovalex@mail.ru>
2019-10-25 23:10:28 +0300

    Scripted auto-commit on change (2019-10-25 23:10:28) by gitwatch.sh

--- src/main/java/bmstu/lab2/testClass.java
+++ src/main/java/bmstu/lab2/testClass.java
@@ -1,5 +1,9 @@
 package bmstu.lab2;
 
+import org.apache.hadoop.io.Text;
+
+import java.util.ArrayList;
+
 import java.io.FileReader;
 import java.util.Arrays;
 import au.com.bytecode.opencsv.CSVReader;
@@ -7,19 +11,24 @@ import au.com.bytecode.opencsv.CSVReader;
 public class testClass {
     public static void main(String[] args) throws Exception {
 
-        //664600583_T_ONTIME_sample.csv
-        //L_AIRPORT_ID.csv
-        CSVReader reader = new CSVReader(new FileReader("664600583_T_ONTIME_sample.csv"), ',' , '"' , 1);
+//        //664600583_T_ONTIME_sample.csv
+//        //L_AIRPORT_ID.csv
+//        CSVReader reader = new CSVReader(new FileReader("664600583_T_ONTIME_sample.csv"), ',' , '"' , 1);
+//
+//        String[] nextLine;
+//        int count = 0;
+//        while ((nextLine = reader.readNext()) != null && count <= 10) {
+//            if (nextLine != null) {
+//                //Verifying the read data here
+//                System.out.println(Arrays.toString(nextLine));
+//            }
+//            count++;
+//        }
 
-        String[] nextLine;
-        int count = 0;
-        while ((nextLine = reader.readNext()) != null && count <= 10) {
-            if (nextLine != null) {
-                //Verifying the read data here
-                System.out.println(Arrays.toString(nextLine));
-            }
-            count++;
-        }
-        //test auto-commit
+        Text text = 
+
+        ReaderCSV check = new ReaderCSV();
+
+
     }
 }
